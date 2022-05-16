0d3f42a3fe866b6306b052dc4909674f0c4bea79
Vladislav Chernykh <vladislav_chernykh@epam.com>
2022-05-16 10:12:27 +0200

    Add a new endpoint for a geozone + courier id retrieval

--- service/apis/geozones.py
+++ service/apis/geozones.py
@@ -3,7 +3,9 @@ from collections import namedtuple
 from flask import Blueprint, request
 from pydantic import BaseModel, StrictStr
 
-from service.src.geozones import add_geozone, set_zone_to_courier_bond
+from service.src.geozones import add_geozone
+from service.src.geozones import set_zone_to_courier_bond
+from service.src.geozones import get_delivery_data_by_coordinates
 
 Coordinates = namedtuple("Coordinates", ['latitude', 'longitude'])
 
@@ -17,7 +19,7 @@ class GeozoneCreateData(BaseModel):
 
 class CourierToDistrictMapData(BaseModel):
     courier_id: int = None
-    district_id: int = None
+    geozone_id: int = None
 
 
 @geozones.route("/add", methods=["POST"])
@@ -38,7 +40,17 @@ def update_zone():
     input_request = request.get_json()
     try:
         data = CourierToDistrictMapData(**input_request)
-        result = set_zone_to_courier_bond(data.courier_id, data.district_id)
+        result = set_zone_to_courier_bond(data.courier_id, data.geozone_id)
         return result["ref"].value["id"]
+    except ValueError as error:
+        return str(error), 500
+
+
+@geozones.route("/getAvailableCourier", methods=["POST"])
+def get_available_courier():
+    """Get available courier and geozone id by passing coordinates"""
+    coords = request.get_json()
+    try:
+        return get_delivery_data_by_coordinates(coords)
     except ValueError as error:
         return str(error), 500
--- service/src/geozones.py
+++ service/src/geozones.py
@@ -1,11 +1,13 @@
 from datetime import datetime
+from typing import Optional
 
 from faunadb import query as q
+from shapely.geometry import Point, Polygon
 
 from service.src.common import get_client
 
 
-def add_geozone(name: str, coordinates: list[tuple[float, float]]):
+def add_geozone(name: str, coordinates: list[tuple[float, float]]) -> dict:
     """Add geozone by name and coordinates"""
     client = get_client()
     result = client.query(
@@ -15,7 +17,7 @@ def add_geozone(name: str, coordinates: list[tuple[float, float]]):
     return result
 
 
-def set_zone_to_courier_bond(courier_id: int, geozone_id: int):
+def set_zone_to_courier_bond(courier_id: int, geozone_id: int) -> dict:
     """Set a zone to courier dependency with a short TTL"""
     client = get_client()
     result = client.query(
@@ -28,3 +30,40 @@ def set_zone_to_courier_bond(courier_id: int, geozone_id: int):
     )
 
     return result
+
+
+def get_delivery_data_by_coordinates(point_coordinates: [float, float]) -> dict:
+    """Find an available courier and return geozone id"""
+    client = get_client()
+    all_geozones = client.query(
+        q.map_(
+            q.lambda_("x", q.get(q.var("x"))),
+            q.paginate(q.match(q.index('all_geozones'))),
+        )
+    )
+    available_geozone_id = _get_available_geozone_id(point_coordinates, all_geozones["data"])
+    if not available_geozone_id:
+        raise ValueError("No available geozone was found")
+
+    geozone_to_courier_map = client.query(
+        q.get(q.match(q.index("all_geozones"), available_geozone_id))
+    )
+    available_courier_id = geozone_to_courier_map["ref"].value["courier_id"] if geozone_to_courier_map else None
+
+    return dict(courier_id=available_courier_id, geozone_id=available_geozone_id)
+
+
+def _get_available_geozone_id(point_coordinates: [float, float], all_geozones: any) -> Optional[int]:
+    """Find an available geozone id"""
+    point = Point(point_coordinates)
+    for geozone in all_geozones:
+        geozone_id = geozone["ref"].value["id"]
+        coords = geozone["data"]["coordinates"]
+        if len(coords) < 3:
+            print(f'Not enough coordinates for a geozone id {geozone_id}.')
+            continue
+
+        polygon = Polygon(coords)
+        if point.within(polygon):
+            return geozone_id
+    return None
